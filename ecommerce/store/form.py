b914ef0a1739b9a267bb3fbf522cffc15d8b5f39
DuyTung <dtung124@gmail.com>
2023-01-06 16:07:45 +0700

    Signed-off-by: DuyTung <dtung124@gmail.com>

--- ecommerce/store/form.py
+++ ecommerce/store/form.py
@@ -3,10 +3,10 @@ import re
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 class RegistrationForm(forms.Form):
-    username = forms.CharField(label='Tài khoản', max_length=30)
+    username = forms.CharField(label='Username', max_length=30)
     email = forms.EmailField(label='Email')
-    password1 = forms.CharField(label='Mật khẩu', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Nhập lại mật khẩu', widget=forms.PasswordInput())
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
 
     def clean_password2(self):
         if "password1" in self.cleaned_data:
@@ -14,17 +14,17 @@ class RegistrationForm(forms.Form):
             password2 = self.cleaned_data['password2']
             if password1 == password2 and password1:
                 return password2
-        raise forms.ValidationError('Mật khẩu không hợp lệ')
+        raise forms.ValidationError('Password is incorrect')
 
     def clean_username(self):
         username = self.cleaned_data['username']
         if not re.search(r'^\w+$',username):
-            raise forms.ValidationError('Tên tài khoản có ký tự đặc biệt')
+            raise forms.ValidationError('Username has a special character')
         try:
             User.objects.get(username=username)
         except ObjectDoesNotExist:
             return username
-        raise forms.ValidationError('Tên tài khoản đã tồn tại')
+        raise forms.ValidationError('Username has existed')
 
     def save(self):
         User.objects.create_user(username=self.cleaned_data['username'], email=self.cleaned_data['email'], password=self.cleaned_data['password1'])
